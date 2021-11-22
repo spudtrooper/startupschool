dc3bb40d8348597b76b1491cb4ab23c5feb70893
Jeff Palm <jeff@jeffpalm.com>
2021-11-22 09:10:31 -0500

    add images, backfill, campany info

--- startupschool/linkify.go
+++ startupschool/linkify.go
@@ -3,13 +3,11 @@ package startupschool
 import "regexp"
 
 var (
-	screenNameRE = regexp.MustCompile(`@([a-zA-Z_0-9]+)`)
-	urlRE        = regexp.MustCompile(`(https?://)([\.a-zA-Z_0-9/\-]+)`)
+	urlRE = regexp.MustCompile(`(https?://)([\.a-zA-Z_0-9/\-]+)`)
 )
 
 func linkify(text string) string {
 	html := text
 	html = urlRE.ReplaceAllString(html, `<a target="_" href="$1$2">$2</a>`)
-	html = screenNameRE.ReplaceAllString(html, `<a target="_" href="https://twitter.com/$1">@$1</a>`)
 	return html
 }
--- startupschool/linkify_test.go
+++ startupschool/linkify_test.go
@@ -21,11 +21,6 @@ func TestLinkify(t *testing.T) {
 			want:  "blah blah blah",
 		},
 		{
-			name:  "name link",
-			input: "@bob",
-			want:  `<a target="_" href="https://twitter.com/bob">@bob</a>`,
-		},
-		{
 			name:  "http link",
 			input: "http://foo-bar_baz.com",
 			want:  `<a target="_" href="http://foo-bar_baz.com">foo-bar_baz.com</a>`,
@@ -34,21 +29,6 @@ func TestLinkify(t *testing.T) {
 			name:  "https link",
 			input: "https://foo.com",
 			want:  `<a target="_" href="https://foo.com">foo.com</a>`,
-		},
-		{
-			name:  "LiN_K",
-			input: "@B_oo00oBB",
-			want:  `<a target="_" href="https://twitter.com/B_oo00oBB">@B_oo00oBB</a>`,
-		},
-		{
-			name:  "links",
-			input: "@bob https://baz.com @sue http://foo.com",
-			want:  `<a target="_" href="https://twitter.com/bob">@bob</a> <a target="_" href="https://baz.com">baz.com</a> <a target="_" href="https://twitter.com/sue">@sue</a> <a target="_" href="http://foo.com">foo.com</a>`,
-		},
-		{
-			name:  "links no space",
-			input: "@bob@sue",
-			want:  `<a target="_" href="https://twitter.com/bob">@bob</a><a target="_" href="https://twitter.com/sue">@sue</a>`,
 		},
 	}
 	for _, test := range tests {
